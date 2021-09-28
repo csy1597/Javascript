13a116d3de32292180eb3b5668169d09ad7b4dc1
csy1597 <qlseo3@naver.com>
2021-09-29 00:03:37 +0900

    javascript

---
+++ javascript.js
@@ -0,0 +1,16 @@
+// 자바스크립트 연산자
+
+const num1 = 15;
+const num2 = 2;
+
+const result1 = num1 + num2;    // 17
+const result2 = num1 - num2;    // 13 
+const result3 = num1 * num2;    // 30
+const result4 = num1 / num2;    // 7.5
+const result5 = num1 % num2;    // 1
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
