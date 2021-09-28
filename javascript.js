f139c5478a338ade88c265e60b5b8ac65cb4762b
csy1597 <qlseo3@naver.com>
2021-09-29 00:41:05 +0900

    if

--- javascript.js
+++ javascript.js
@@ -14,3 +14,18 @@ console.log(result2);
 console.log(result3);
 console.log(result4);
 console.log(result5);
+
+// 제어문
+// if, else if, else
+
+const name = 'cha';
+if(name === 'cha'){
+    console.log('Welcome chacha!!');
+}else if(name='seung'){
+    console.log('Welcome seung !!');
+} else{
+    console.log("i don't no");
+}
+
+// 삼항연산자
+console.log(name==='cha' ? 'yes' : 'no');
