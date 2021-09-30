ac0e0295bdc6d7821efafab6154ff660993e011e
csy1597 <qlseo3@naver.com>
2021-10-01 01:23:13 +0900

    object5

--- Object.js
+++ Object.js
@@ -62,3 +62,20 @@ function makeUser(name, age){
     this.age = age;
     // return this; 를 생략한 것이다.
 }
+
+// 5. for..in vs for..of
+// for (key in obj)
+console.clear();    // 콘솔창에 이전에 있던 값을 지워주는 것 
+for (key in userInfo){
+    console.log(key);
+}
+
+// for(value of iterable) 배열이나 리스트
+const arr = [1,2,3,4];
+for(let i =0; i<arr.length;i++){
+    console.log(arr[i]);
+}
+for (value of array){
+    console.log(value)
+}
+
