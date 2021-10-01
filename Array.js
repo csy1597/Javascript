c5249ebf9fb5a7753349d739515100ea4267f598
csy1597 <qlseo3@naver.com>
2021-10-02 00:53:32 +0900

    index

---
+++ Array.js
@@ -0,0 +1,17 @@
+// 배열
+
+// 1. 배열 선언
+const arr1 = new Array();
+const arr2 = [1,2];
+
+//2. index position
+const fruits = ['apple', 'banana'];
+console.log(fruits);            // ['apple', 'banana']
+console.log(fruits.length);     // 2
+console.log(fruits[0]);         // apple
+console.log(fruits[1]);         // banana
+console.log(fruits[2]);         // undefined
+
+console.log(fruits[fruits.length - 1]);     // 배열에서 제일 마지막에 값이 궁금할 때 사용    
+
+
