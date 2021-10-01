022460bb6bcacd3bf6ee85b684f685f8e8fc385d
csy1597 <qlseo3@naver.com>
2021-10-02 01:03:59 +0900

    for

--- Array.js
+++ Array.js
@@ -14,4 +14,24 @@ console.log(fruits[2]);         // undefined
 
 console.log(fruits[fruits.length - 1]);     // 배열에서 제일 마지막에 값이 궁금할 때 사용    
 
+// 3. looping over an array
+// for 
+for (let i = 0; i < fruits.length; i++){
+    console.log(fruits[i]);
+}
+
+// for of 
+for (let fruit of fruits){
+    console.log(fruit);
+}
+
+//forEach
+fruits.forEach(function (fruit, indexm, array){
+    console.log(fruit);
+});
+// 화살표함수
+fruits.forEach( (fruit) => console.log(fruit)  );
+
+
+
 
