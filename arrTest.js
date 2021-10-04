07fc77e96fa4f5ef57ec9dd1b60f5e799a084fe2
csy1597 <qlseo3@naver.com>
2021-10-04 21:10:30 +0900

    arrtest

--- arrTest.js
+++ arrTest.js
@@ -32,18 +32,36 @@ const carsNames = cars.map(function(car){
 });
 console.log(carsNames);
 
+console.log("-----------------------------")
 // 3. filter -> 배열 안에서 특정조건을 가지고 잇는 데이터를 출력하는놈
 const carSize = cars.filter(function(car){
     return car.size === '대형';
 })
 console.log(carSize);
 
+console.log("-----------------------------")
 // 4. reduce -> 배열 안의 값을 합하는 것이다.
 
+// 기본적인 사용법
 const number = [1,2,3,4];
 const total = number.reduce(function(a, b){
     console.log(a,b);
     return a+b;
 })
-
 console.log(total);
+
+console.log("-----------------------------")
+// 데이터가 object 일때 
+const scores = [
+    {name:'영어', score:50}
+    , {name:'수학', score:45}
+    , {name:'국어', score:80}
+    , {name:'역사', score:100}
+];
+
+const scoreTotal = scores.reduce(function(a,b){
+    return a + b.score;
+}, 0);
+
+console.log(scoreTotal);
+
